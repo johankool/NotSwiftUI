fa6e41e0c6fb4842fd0bf1524240a2d0817f47b4
Johan Kool <johankool@users.noreply.github.com>
2024-11-09 12:38:14 +0100

    Transaction stubs

---
+++ Sources/NotSwiftUI/Transaction.swift
@@ -0,0 +1,38 @@
+//
+//  Transaction.swift
+//  NotSwiftUI
+//
+//  Created by Johan Kool on 09/11/2024.
+//
+
+
+@frozen public struct Transaction {
+
+    /// Creates a transaction.
+    @inlinable public init() {
+        
+    }
+
+    /// Accesses the transaction value associated with a custom key.
+    ///
+    /// Create custom transaction values by defining a key that conforms to the
+    /// ``TransactionKey`` protocol, and then using that key with the subscript
+    /// operator of the ``Transaction`` structure to get and set a value for
+    /// that key:
+    ///
+    ///     private struct MyTransactionKey: TransactionKey {
+    ///         static let defaultValue = false
+    ///     }
+    ///
+    ///     extension Transaction {
+    ///         var myCustomValue: Bool {
+    ///             get { self[MyTransactionKey.self] }
+    ///             set { self[MyTransactionKey.self] = newValue }
+    ///         }
+    ///     }
+//    public subscript<K>(key: K.Type) -> K.Value where K : TransactionKey
+}
+
+public func withTransaction<Result>(_ transaction: Transaction, _ body: () throws -> Result) rethrows -> Result {
+    fatalError()
+}
